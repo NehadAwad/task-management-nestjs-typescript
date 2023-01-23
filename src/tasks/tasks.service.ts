459acb70d8c615d3afd02eb47cc7c3dab73ec7cf
nehadawad <nehadakash622@gmail.com>
2023-01-23 19:19:09 +0600

    get task by id service created

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import * as uuid from 'uuid';
+import { createTaskDto } from './dto/create-task.dto';
 const newUuid = uuid.v1() 
 
 @Injectable()
@@ -11,7 +12,12 @@ export class TasksService {
         return this.tasks;  
     }
 
-    createTask(title:string, description:string): Task {
+    getTaskById(id: string): Task {
+        return this.tasks.find(task => task.id === id);
+    }
+
+    createTask(createTaskDto: createTaskDto): Task {
+        const { title, description } = createTaskDto;
         const task: Task = {
             id: newUuid,
             title,
@@ -22,4 +28,5 @@ export class TasksService {
         this.tasks.push(task);
         return task;
     }
+
 }
